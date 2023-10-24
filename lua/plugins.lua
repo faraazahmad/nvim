1073b35c3dd33d0a60465f170d4ccfd0d0e67722
Syed Faraaz Ahmad <faraaz98@live.com>
2023-10-25 00:18:22 +0530

    update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,16 +1,10 @@
 -- install packer if not present
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+	fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
 end
-
-local packer_bootstrap = ensure_packer()
 
 -- autorun PackerCompile when this file is modified
 vim.cmd([[
@@ -67,32 +61,29 @@ return require('packer').startup(function(use)
 	}
 
 	-- treesitter
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use { 'nvim-treesitter/nvim-treesitter' }
 
 	-- nvim lsp stuff
+	-- Mason LSP manager
+	use {'williamboman/mason.nvim'} 
+	use {'williamboman/mason-lspconfig.nvim'} 
 	use {
   		'VonHeikemen/lsp-zero.nvim',
   		branch = 'v3.x',
-  		requires = {
-    
-    		{'williamboman/mason.nvim'},           
-    		{'williamboman/mason-lspconfig.nvim'},
-
-			-- LSP Support
-    		{'neovim/nvim-lspconfig'},    -- Required
-
-    		-- Autocompletion
-	    	{'hrsh7th/nvim-cmp'},         -- Required
-    		{'hrsh7th/cmp-nvim-lsp'},     -- Required
-	    	{'hrsh7th/cmp-buffer'},       -- Optional
-			{'hrsh7th/cmp-path'},         -- Optional
-		    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		    {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-    		-- Snippets
-			{'L3MON4D3/LuaSnip'},             -- Required
-    		{'rafamadriz/friendly-snippets'}, -- Optional
-  		}
+		lazy = true,
+		config = false
+	}
+	use {
+		'neovim/nvim-lspconfig',
+		requires = {
+			'hrsh7th/cmp-nvim-lsp'
+		}
+	}
+	use {
+		'hrsh7th/nvim-cmp',
+		requires = {
+			'L3MON4D3/LuaSnip'	
+		}
 	}
 
 	-- themes
